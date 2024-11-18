db5fadc809acfe4008e9e98575b2a28cc56c2f88
KaikeMotta <161529747+KaikeMotta@users.noreply.github.com>
2024-11-18 13:26:18 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -3,7 +3,7 @@ package database
 import (
 	"log"
 	"os"
-	"github.com/guilhermeonrails/api-go-gin/models"
+	"github.com/alura-cursos/Curso_CI/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,7 +14,6 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	
 	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
